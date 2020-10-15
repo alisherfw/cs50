15eb7b8a13c36575aa138d51e5e73800c16d4c7b
alisherfw <57340324+alisherfw@users.noreply.github.com>
2020-10-16 01:10:08 +0500

    cough function

--- C++/cough3/cough3.cpp
+++ C++/cough3/cough3.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 using namespace std;
 
+void cough(int n);
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    cough(7);
+}
+void cough(int n) {
+    for (int i = 0; i < n; i++) {
+        cout << "Cough!" << endl;
+    }
 }
