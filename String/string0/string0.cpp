6aa528f598b8c8362a72d71705cda19989440ac3
alisherfw <57340324+alisherfw@users.noreply.github.com>
2020-10-16 22:17:05 +0500

    ASCII changed to cctype library

--- String/string0/string0.cpp
+++ String/string0/string0.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <string>
+#include <cctype>
+
 using namespace std;
 int main() {
 	string msg;
 	cin >> msg;
 	for (int i = 0; i <= msg.length(); i++) {
 		if (msg[i] >= 'a' && msg[i] <= 'z') {
-			char character = msg[i] - 32;
+			 char character = toupper(msg[i]);
 			cout << character;
 		} 
 		else {
