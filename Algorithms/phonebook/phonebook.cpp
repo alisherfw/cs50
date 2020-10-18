5e6f456b021361bde6581c1bd947c676457c188f
alisherfw <57340324+alisherfw@users.noreply.github.com>
2020-10-18 18:42:23 +0500

    phonebook changed to struct

--- Algorithms/phonebook/phonebook.cpp
+++ Algorithms/phonebook/phonebook.cpp
@@ -1,16 +1,35 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+typedef struct {
+	string name;
+	string number;
+} person;
+
 int main() {
-	string names[4] = { "EMMA", "RODRIGO", "BRIAN", "DAVID" };
-	string numbers[4] = { "617-4232-3232", "617-2321-2351", "617-123-3456", "617-423-1233" };
+	
+	person contacts[4];
+
+	contacts[0].name = "EMMA";
+	contacts[0].number = "617-123-0100";
+
+	contacts[1].name = "RODRIGO";
+	contacts[1].number = "617-321-5555";
+
+	contacts[2].name = "BRIAN";
+	contacts[2].number = "617-321-4040";
+
+	contacts[3].name = "DAVID";
+	contacts[3].number = "617-777-7777";
+	 
 
 	string input;
 	cin >> input;
 
 	for (int i = 0; i < 4; i++) {
-		if (names[i] == input) {
-			cout << names[i] << ": " << numbers[i] << endl;
+		if (contacts[i].name == input) {
+			cout << contacts[i].name << ": " << contacts[i].number << endl;
 			return 0;
 		}
 	}
